cdc0b3c88f44258a24d8319a1b07f9552e48f021
joewen <wennjoe@163.com>
2017-06-25 17:57:42 +0800

    练习6 [1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,44,555,666,65555,332,444,22252] 求这个list的最大值

--- for循环.py
+++ for循环.py
@@ -7,8 +7,18 @@
 # for num in range(0,20):
 #     print(num)
 
-times = 0
-for i in ['C','js','python','js','css','js','html','node']:
-    if i == 'js':
-        times += 1
-print(times)
+# times = 0
+# for i in ['C','js','python','js','css','js','html','node']:
+#     if i == 'js':
+#         times += 1
+# print(times)
+
+#可以用列表排序来写，也可以用for迭代方式，将list里面的元素进行比较。取大数
+list = [1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,44,555,666,65555,332,444,22252]
+# list.sort(reverse=True)
+# print(list[0])
+s = 0
+for i in list:
+    if i > s:
+        s = i
+print(s)
