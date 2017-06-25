df022513bf80da19c971b29196ab0eec1ac30e7b
joewen <wennjoe@163.com>
2017-06-25 11:04:36 +0800

    add not 0 判断

--- while.py
+++ while.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
-#auther: Joe wen
-i = 0
-while i < 20:
-    print(i)
-    i += 1  #这里相当于i = i+1
+# #auther: Joe wen
+# i = 0
+# while i < 20:
+#     print(i)
+#     i += 1  #这里相当于i = i+1
+#
+#
+# name = ''
+# while not name:
+#     name = input("input name is: ")
+# print('my name is:',name)
 
-
-name = ''
-while not name:
-    name = input("input name is: ")
-print('my name is:',name)
+s = 0
+number1 = ''
+while number1 != 0:
+    number1 = int(input("input nubmer is:"))
+    if number1 == 0:
+        break
+    s = number1+s
+    print(s)
