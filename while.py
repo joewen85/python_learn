47e6f4c4ba6ea485722b39cb4accdda8fef7a39f
joewen <wennjoe@163.com>
2017-06-25 10:43:33 +0800

    add empty variable

--- while.py
+++ while.py
@@ -3,5 +3,10 @@
 i = 0
 while i < 20:
     print(i)
-    #i += 1  #这里相当于i = i+1
-    i = i+1
+    i += 1  #这里相当于i = i+1
+
+
+name = ''
+while not name:
+    name = input("input name is: ")
+print('my name is:',name)
